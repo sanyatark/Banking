8d15340bc9701125456efb004bfedbf23930b967
Tark <sanyatark@gmail.com>
2015-07-23 22:51:34 +0800

    Create script.rb

---
+++ script.rb
@@ -0,0 +1,32 @@
+class Account
+attr_reader :name, :balance
+
+def initialize(name, balance = 100) 
+    @name = name
+    @balance = balance 
+end
+
+
+private
+
+def pin
+    @pin = 1234
+end
+
+def pin_error
+    return "Access denied: incorrect PIN."
+end
+
+
+public
+
+def display_balance(pin_number)
+    puts pin_number == pin ?  "Balance: $#{@balance}." : pin_error
+end
+
+def withdraw(pin_number, amount)
+    puts pin_number == pin ? "Withdrew #{amount}. New balance: $#{@balance}." : pin_error
+end
+end
+
+checking_account = Account.new("Tark", 888) 
